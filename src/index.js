4f4c7a3d4aa897f0f328ea32b408cd84b2b26dcb
Radovan <radosce0@gmail.com>
2024-12-20 14:07:07 +0100

    Update routes

--- src/index.js
+++ src/index.js
@@ -1,14 +1,14 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom/client';
 import { ThemeProvider } from '@mui/material/styles';
 import CssBaseline from '@mui/material/CssBaseline';
 import App from './App';
 import theme from './theme';
 
-ReactDOM.render(
+const root=ReactDOM.createRoot(document.getElementById('root'));
+root.render(
   <ThemeProvider theme={theme}>
     <CssBaseline />
     <App />
-  </ThemeProvider>,
-  document.getElementById('root')
+  </ThemeProvider>
 );
